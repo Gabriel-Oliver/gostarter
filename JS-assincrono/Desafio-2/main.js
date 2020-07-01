2375c9009995a6334e5052f3d35ae10daaf806f1
Gabriel Óliver <gabrielmed2010@hotmail.com>
2020-07-01 15:52:05 -0300

    adiciona o exercício 1 da lista de desafios

---
+++ JS-assincrono/Desafio-2/main.js
@@ -0,0 +1,24 @@
+function adicionar() {
+  let inputElement = document.querySelector("input");
+  nameuser = inputElement.value;
+  let nameURL = "https://api.github.com/users/" + nameuser + "/repos";
+  axios
+    .get(nameURL)
+    .then(function (response) {
+      let ul = document.querySelector("#list");
+      let li;
+      li = document.createElement("ul");
+      li.innerText = nameuser;
+      ul.appendChild(li);
+
+      for (let i = 0; i < response.data.length; i++) {
+        li = document.createElement("li");
+        li.innerText = response.data[i].name;
+
+        ul.appendChild(li);
+      }
+    })
+    .catch(function (error) {
+      console.warn(error);
+    });
+}
