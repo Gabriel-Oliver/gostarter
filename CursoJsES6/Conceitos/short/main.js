ae639310ecd599cddd97aff8d6e39f9cc9c293bd
Gabriel Óliver <gabrielmed2010@hotmail.com>
2020-07-14 17:08:23 -0300

    Introduz a utilização de object short syntax

---
+++ CursoJsES6/Conceitos/short/main.js
@@ -0,0 +1,10 @@
+const name = "Gabriel";
+const age = 20;
+
+const user = {
+  name,
+  age,
+  company: "Nenhuma",
+};
+
+console.log(user);
---
+++ CursoJsES6/Conceitos/short/shortbundle.js
@@ -0,0 +1,10 @@
+"use strict";
+
+var name = "Gabriel";
+var age = 20;
+var user = {
+  name: name,
+  age: age,
+  company: "Nenhuma"
+};
+console.log(user);
