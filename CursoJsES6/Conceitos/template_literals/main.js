cd959ade50f59300a5ae87e053def785b1488a5a
Gabriel Óliver <gabrielmed2010@hotmail.com>
2020-07-14 17:00:39 -0300

    Introduz a utilização do template literals

---
+++ CursoJsES6/Conceitos/template_literals/main.js
@@ -0,0 +1,4 @@
+const name = "Gabriel";
+const age = "20";
+
+console.log(`Meu nome é ${name} e tenho ${age} anos`);
---
+++ CursoJsES6/Conceitos/template_literals/templatebundle.js
@@ -0,0 +1,5 @@
+"use strict";
+
+var name = "Gabriel";
+var age = "20";
+console.log("Meu nome \xE9 ".concat(name, " e tenho ").concat(age, " anos"));
