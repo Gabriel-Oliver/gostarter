6f8a3092491aa153e63d5c24a617bc5b7ff1d0d7
Gabriel Óliver <gabrielmed2010@hotmail.com>
2020-07-16 16:40:01 -0300

    adiciona o exercício 3.1 da lista de desafios

---
+++ CursoJsES6/Conceitos/Exercicio3/bundle.js
@@ -0,0 +1,8 @@
+"use strict";
+
+// 3.1
+var arr = [1, 2, 3, 4, 5];
+var arr2 = arr.map(function (item) {
+  return item + 10;
+});
+console.log(arr2);
---
+++ CursoJsES6/Conceitos/Exercicio3/main.js
@@ -0,0 +1,4 @@
+// 3.1
+const arr = [1, 2, 3, 4, 5];
+const arr2 = arr.map((item) => item + 10);
+console.log(arr2);
