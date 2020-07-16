7e29d2a35b6941de12c7f3c4ae47a205361236cc
Gabriel Óliver <gabrielmed2010@hotmail.com>
2020-07-16 18:08:36 -0300

    adiciona o exercício 3.3 da lista de desafios

--- CursoJsES6/Conceitos/Exercicio3/bundle.js
+++ CursoJsES6/Conceitos/Exercicio3/bundle.js
@@ -6,9 +6,6 @@ var arr2 = arr.map(function (item) {
   return item + 10;
 }); // console.log(arr2);
 //git commit -m "adiciona o exercício 3.1 da lista de desafios"
-//git commit -m "adiciona o exercício 3.2 da lista de desafios"
-//git commit -m "adiciona o exercício 3.3 da lista de desafios"
-//git commit -m "adiciona o exercício 3.4 da lista de desafios"
 
 var usuario = [{
   nome: "Diego",
@@ -16,5 +13,16 @@ var usuario = [{
 }];
 var test = usuario.map(function (usuario) {
   return usuario.idade;
+}); // console.log(test);
+//git commit -m "adiciona o exercício 3.2 da lista de desafios"
+
+var usuario2 = [{
+  nome: "Diego",
+  idade: 23
+}];
+var teste = usuario2.find(function (usuario2) {
+  return usuario2;
 });
-console.log(test);
+console.log(teste.nome, teste.idade);
+console.log(teste.idade); //git commit -m "adiciona o exercício 3.3 da lista de desafios"
+//git commit -m "adiciona o exercício 3.4 da lista de desafios"
--- CursoJsES6/Conceitos/Exercicio3/main.js
+++ CursoJsES6/Conceitos/Exercicio3/main.js
@@ -4,11 +4,21 @@ const arr = [1, 2, 3, 4, 5];
 const arr2 = arr.map((item) => item + 10);
 
 // console.log(arr2);
+
 //git commit -m "adiciona o exercício 3.1 da lista de desafios"
-//git commit -m "adiciona o exercício 3.2 da lista de desafios"
-//git commit -m "adiciona o exercício 3.3 da lista de desafios"
-//git commit -m "adiciona o exercício 3.4 da lista de desafios"
 
 const usuario = [{ nome: "Diego", idade: 23 }];
 const test = usuario.map((usuario) => usuario.idade);
-console.log(test);
+// console.log(test);
+
+//git commit -m "adiciona o exercício 3.2 da lista de desafios"
+
+const usuario2 = [{ nome: "Diego", idade: 23 }];
+
+const teste = usuario2.find((usuario2) => usuario2);
+console.log(teste.nome, teste.idade);
+console.log(teste.idade);
+
+//git commit -m "adiciona o exercício 3.3 da lista de desafios"
+
+//git commit -m "adiciona o exercício 3.4 da lista de desafios"
