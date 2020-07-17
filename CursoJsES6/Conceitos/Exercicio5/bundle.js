c3ef8bac3d2435ed410c871fe05a4300b9b0b165
Gabriel Óliver <gabrielmed2010@hotmail.com>
2020-07-17 18:16:02 -0300

    adiciona o exercício 5.2 da lista de desafios (Spread) incompleto

--- CursoJsES6/Conceitos/Exercicio5/bundle.js
+++ CursoJsES6/Conceitos/Exercicio5/bundle.js
@@ -1,5 +1,11 @@
 "use strict";
 
+function ownKeys(object, enumerableOnly) { var keys = Object.keys(object); if (Object.getOwnPropertySymbols) { var symbols = Object.getOwnPropertySymbols(object); if (enumerableOnly) symbols = symbols.filter(function (sym) { return Object.getOwnPropertyDescriptor(object, sym).enumerable; }); keys.push.apply(keys, symbols); } return keys; }
+
+function _objectSpread(target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i] != null ? arguments[i] : {}; if (i % 2) { ownKeys(Object(source), true).forEach(function (key) { _defineProperty(target, key, source[key]); }); } else if (Object.getOwnPropertyDescriptors) { Object.defineProperties(target, Object.getOwnPropertyDescriptors(source)); } else { ownKeys(Object(source)).forEach(function (key) { Object.defineProperty(target, key, Object.getOwnPropertyDescriptor(source, key)); }); } } return target; }
+
+function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
+
 var arr = [1, 2, 3, 4, 5, 6];
 var x = arr[0],
     y = arr.slice(1); // console.log(x);
@@ -15,3 +21,28 @@ function soma() {
   });
 } // console.log(soma(1, 2));
 // git commit -m "adiciona o exercício 5.1 da lista de desafios (Rest)"
+
+
+var usuario = {
+  nome: "Diego",
+  idade: 23,
+  endereco: {
+    cidade: "Rio do Sul",
+    uf: "SC",
+    pais: "Brasil"
+  }
+};
+
+var usuario2 = _objectSpread(_objectSpread({}, usuario), {}, {
+  nome: "Gabriel"
+});
+
+console.log(usuario2);
+
+var usuario3 = _objectSpread(_objectSpread({}, usuario), {}, {
+  endereco: {
+    cidade: "Lontras"
+  }
+});
+
+console.log(usuario3); // git commit -m "adiciona o exercício 5.2 da lista de desafios (Spread)"
--- CursoJsES6/Conceitos/Exercicio5/main.js
+++ CursoJsES6/Conceitos/Exercicio5/main.js
@@ -10,4 +10,24 @@ function soma(...numeros) {
 }
 
 // console.log(soma(1, 2));
+
 // git commit -m "adiciona o exercício 5.1 da lista de desafios (Rest)"
+
+const usuario = {
+  nome: "Diego",
+  idade: 23,
+  endereco: {
+    cidade: "Rio do Sul",
+    uf: "SC",
+    pais: "Brasil",
+  },
+};
+
+const usuario2 = { ...usuario, nome: "Gabriel" };
+
+console.log(usuario2);
+
+const usuario3 = { ...usuario, endereco: { cidade: "Lontras" } };
+
+console.log(usuario3);
+// git commit -m "adiciona o exercício 5.2 da lista de desafios (Spread)"
