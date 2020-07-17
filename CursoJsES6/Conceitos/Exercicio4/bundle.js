d774693fd6d0dd8e88aa85d915fe18b752d4c834
Gabriel Óliver <gabrielmed2010@hotmail.com>
2020-07-17 17:06:21 -0300

    adiciona o exercício 4.1 da lista de desafios

---
+++ CursoJsES6/Conceitos/Exercicio4/bundle.js
@@ -0,0 +1,18 @@
+"use strict";
+
+var empresa = {
+  nome: "Rocketseat",
+  endereco: {
+    cidade: "Rio do Sul",
+    estado: "SC"
+  }
+};
+var nome = empresa.nome,
+    _empresa$endereco = empresa.endereco,
+    cidade = _empresa$endereco.cidade,
+    estado = _empresa$endereco.estado;
+console.log(nome); // Rocketseat
+
+console.log(cidade); // Rio do Sul
+
+console.log(estado); // SC
