f960dd8cf6d77e189632f478993099a2992f9a3b
Gabriel Óliver <gabrielmed2010@hotmail.com>
2020-07-20 14:58:00 -0300

    adiciona as funcionalidades do export e do import

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
-import { soma } from "./functions";
+import * as functions from "./functions";
+// import somaFunction from "./soma";
 
-console.log(soma(1, 2));
+console.log(functions);
+console.log(functions.soma(1, 2));
+console.log(functions.sub(4, 2));
+console.log(functions.mult(4, 2));
