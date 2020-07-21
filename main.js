7bdfc4d1f4a6ee53b6e4e25ae0791774d9f0ce04
Gabriel Óliver <gabrielmed2010@hotmail.com>
2020-07-21 17:43:29 -0300

    adiciona a funcionalidade de devServer

--- main.js
+++
@@ -1,7 +0,0 @@
-import * as functions from "./functions";
-// import somaFunction from "./soma";
-
-console.log(functions);
-console.log(functions.soma(1, 2));
-console.log(functions.sub(4, 2));
-console.log(functions.mult(4, 2));
