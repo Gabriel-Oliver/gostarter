ebf8bf5b170bc3ea3254a9701bd8e92f040675f8
Gabriel Óliver <gabrielmed2010@hotmail.com>
2020-07-25 17:23:40 -0300

    adiciona a função de async e do await

--- src/main.js
+++ src/main.js
@@ -1,3 +1,19 @@
-import ClasseUsuario, { idade as idadeUsuario } from "./functions";
-ClasseUsuario.info();
-console.log(idadeUsuario);
+// import ClasseUsuario, { idade as idadeUsuario } from "./functions";
+// ClasseUsuario.info();
+// console.log(idadeUsuario);
+
+const minhaPromise = () =>
+  new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve("OK");
+    }, 2000);
+  });
+
+const executaPromise = async () => {
+  console.log(await minhaPromise());
+  console.log(await minhaPromise());
+  console.log(await minhaPromise());
+  console.log(await minhaPromise());
+};
+
+executaPromise();
