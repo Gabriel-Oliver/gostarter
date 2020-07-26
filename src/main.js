0108ca501a78a585ed5f562d46e35381096c28fc
Gabriel Óliver <gabrielmed2010@hotmail.com>
2020-07-26 17:14:16 -0300

    adiciona a terceira parte dos exercícios da lista de desafios do modulo 03

--- src/main.js
+++ src/main.js
@@ -22,11 +22,20 @@
 
 // api.getUserInfo("1SDLASIDH");
 // api.getUserInfo("Gabriel-Oliver");
+
 // DESAFIO 1 MODULO 3
+
 // import { umPorSegundo } from "./indexdesa1";
 // umPorSegundo();
+
 // DESAFIO 2 MODULO 3
 
-import dataUser from "./indexdesa2";
-dataUser.info("diego3g");
-dataUser.info("diego3g124123");
+// import dataUser from "./indexdesa2";
+// dataUser.info("diego3g");
+// dataUser.info("diego3g124123");
+
+// DESAFIO 3 MODULO 3
+
+import Github from "./indexdesa3";
+Github.getRepositories("rocketseat/rocketseat.com.br");
+Github.getRepositories("rocketseat/dslkvmskv");
